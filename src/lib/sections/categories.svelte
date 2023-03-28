76f7c0b28f7db13c4ece2c168340609bda7574a6
Shawn Long <shawnlong636@gmail.com>
2023-03-27 20:21:10 -0700

    feat: added header and subheader for categories

---
+++ src/lib/sections/categories.svelte
@@ -0,0 +1,4 @@
+<div class="grid grid-flow-row justify-items-center items-center">
+	<h3 class="text-md font-bold">Top Categories</h3>
+	<p class="text-sm text-slate-500">Eu non ad elit irure minim enim Lorem</p>
+</div>
--- src/routes/(app)/dev/+page.svelte
+++ src/routes/(app)/dev/+page.svelte
@@ -1,4 +1,10 @@
+<script lang="ts">
+	import Categories from '$lib/sections/categories.svelte';
+</script>
+
 <div class="grid grid-flow-row p-5">
-	<h1 class="text-2xl font-medium">Currently In Development</h1>
-	<p>Insert component here...</p>
+	<h1 class="text-2xl font-medium pb-10">Currently In Development</h1>
+	<div class="bg-slate-200">
+		<Categories />
+	</div>
 </div>
