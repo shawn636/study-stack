f42dfa224ea2f672aa81b32b72a130f42ef7574e
Shawn Long <shawnlong636@gmail.com>
2023-03-27 21:56:39 -0700

    feat: added edge function and dev page

--- src/lib/sections/categories.svelte
+++ src/lib/sections/categories.svelte
@@ -1,4 +1,14 @@
+<script lang="ts">
+	import CategoryCard from '$lib/components/category-card.svelte';
+</script>
+
 <div class="grid grid-flow-row justify-items-center items-center">
 	<h3 class="text-md font-bold">Top Categories</h3>
 	<p class="text-sm text-slate-500">Eu non ad elit irure minim enim Lorem</p>
+</div>
+
+<div class="grid grid-flow-col">
+	{#each Array(6) as category}
+		<CategoryCard />
+	{/each}
 </div>
--- src/routes/(app)/dev/+page.svelte
+++ src/routes/(app)/dev/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="grid grid-flow-row p-5">
 	<h1 class="text-2xl font-medium pb-10">Currently In Development</h1>
-	<div class="bg-slate-200">
+	<div class="border-pink-500 border border-dashed">
 		<Categories />
 	</div>
 </div>
