cf65eb674ce9436aa59016dcf1f7d31eba819684
Shawn Long <shawnlong636@gmail.com>
2023-04-01 10:55:35 -0700

    fix: changes process.env to sveltekit api

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
 	import * as amplitude from '@amplitude/analytics-browser';
 	import '../app.css';
 	import { onMount } from 'svelte';
+	import { PUBLIC_AMPLITUDE_API_KEY } from '$env/static/public';
 
 	onMount(() => {
-		amplitude.init(process.env['AMPLITUDE_API_KEY'] ?? '', undefined, {
+		amplitude.init(PUBLIC_AMPLITUDE_API_KEY, undefined, {
 			defaultTracking: {
 				sessions: true,
 				pageViews: true,
