0827d8d964730d15e86dade296ab121abf61eede
Shawn Long <shawnlong636@gmail.com>
2023-04-01 09:31:06 -0700

    feat: added basic amplitude analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
+	import * as amplitude from '@amplitude/analytics-browser';
 	import '../app.css';
+
+	amplitude.init('739ec8caebbccf02c3c6f26e0657cd24', undefined, {
+		defaultTracking: {
+			sessions: true,
+			pageViews: true,
+			formInteractions: true,
+			fileDownloads: true
+		}
+	});
 </script>
 
 <slot />
