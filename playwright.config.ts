39cbec87683ab9c86936282f0fed1943020e630c
Shawn Long <shawnlong636@gmail.com>
2023-04-29 20:29:21 -0700

    fix: added explicit command to web server command

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run vercel-dev',
+		command: 'PUBLIC_ENV=PROD vercel dev --listen 3000 --token $VERCEL_TOKEN --yes',
 		port: 3000
 	},
 	testDir: 'e2e'
