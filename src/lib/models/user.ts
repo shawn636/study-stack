70f71b25b0e687bfc8e1f10bf02cf9d732c04509
Shawn Long <shawnlong636@gmail.com>
2023-07-08 15:44:28 -0700

    refactor: organized prisma schema and added phone numbers

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -2,4 +2,11 @@ export default interface User {
     id: string;
     email: string;
     name: string;
+    country_code: string | null;
+    area_code: string | null;
+    phone_number: string | null;
+    bio: string | null;
+    city: string | null;
+    state: string | null;
+    photo_url: string | null;
 }
