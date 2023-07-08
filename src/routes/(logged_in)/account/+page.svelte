0942d212021e9e7992567e22b576ba13ed8e5efb
Shawn Long <shawnlong636@gmail.com>
2023-07-07 20:42:02 -0700

    feat: added initial layout to account page

--- src/routes/(logged_in)/account/+page.svelte
+++ src/routes/(logged_in)/account/+page.svelte
@@ -2,62 +2,67 @@
     import { Avatar } from '@skeletonlabs/skeleton';
     import type { PageServerData } from './$types';
     import Fa from 'svelte-fa';
-    import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+    import { faEnvelope, faPhone, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+    import { onMount } from 'svelte';
+    import { formatPhoneNumber } from '$lib/client/util';
 
     export let data: PageServerData;
+    let edit_mode = false;
+    let phone = formatPhoneNumber('9512158574');
+    let bio = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. ';
+    let city = '';
+    let state = '';
+
+    export let original_data: PageServerData;
+    let original_phone = phone;
+    let original_bio = bio;
+    let original_city = city;
+    let original_state = state;
+
+    onMount(() => {
+        original_data = data;
+    });
+
+    const updatePhoneNumber = (
+        event: Event & { currentTarget: EventTarget & HTMLInputElement }
+    ) => {
+        const inputNumber = event.currentTarget.value;
+        phone = formatPhoneNumber(inputNumber);
+        event.currentTarget.value = phone;
+    };
+
+    const cancelChanges = () => {
+        data = original_data;
+
+        // These are only necessary because I haven't added those fields to the db yet
+        // Once they're added, all the needed data will be under data.user
+        phone = original_phone;
+        bio = original_bio;
+        city = original_city;
+        state = original_state;
+
+        edit_mode = false;
+    };
+
+    const saveChanges = () => {
+        // TODO: Call API to send changes to the server
+
+        original_data = data;
+
+        // These are only necessary because I haven't added those fields to the db yet
+        // Once they're added, all the needed data will be under data.user
+        original_phone = phone;
+        original_bio = bio;
+        original_city = city;
+        original_state = state;
+
+        edit_mode = !edit_mode;
+    };
 
     $: initials = data.user.name
         .split(' ')
         .map((n) => n[0])
         .join('');
-
-    let edit_mode = false;
-    let phone = '';
-
-    function formatPhoneNumber(event: any) {
-        let input = event.target;
-        let inputLength = input.value.length;
-        let cursorPosition = input.selectionStart;
-        let isBackspace = event.inputType === 'deleteContentBackward';
-
-        // Remove any non-digit characters from the input
-        phone = input.value.replace(/\D/g, '');
-
-        // Restrict the input to 10 digits
-        phone = phone.slice(0, 10);
-
-        // Format the phone number with spaces or any desired formatting
-        if (phone.length >= 3 && !isBackspace) {
-            phone = `${phone.slice(0, 3)} ${phone.slice(3)}`;
-            cursorPosition += 1; // Adjust cursor position for added space
-        }
-
-        if (phone.length >= 7 && !isBackspace) {
-            phone = `${phone.slice(0, 7)} ${phone.slice(7)}`;
-            cursorPosition += 1; // Adjust cursor position for added space
-        }
-
-        // Handle deletion
-        if (isBackspace && phone.charAt(cursorPosition - 1) === ' ') {
-            // If the previous character is a space, remove both the space and the digit
-            phone = phone.slice(0, cursorPosition - 2) + phone.slice(cursorPosition);
-            cursorPosition -= 1; // Adjust cursor position for removed space
-        } else if (isBackspace && phone.charAt(cursorPosition) === ' ') {
-            // If the next character is a space, remove only the digit
-            phone = phone.slice(0, cursorPosition - 1) + phone.slice(cursorPosition);
-        }
-
-        // Update the input value with the formatted phone number
-        input.value = phone;
-
-        // Adjust cursor position based on the number of spaces
-        let spacesBeforeCursor = (phone.slice(0, cursorPosition).match(/ /g) || []).length;
-        let spacesAfterCursor = (phone.slice(cursorPosition).match(/ /g) || []).length;
-        let adjustedCursorPosition = cursorPosition + spacesBeforeCursor - spacesAfterCursor;
-
-        // Set cursor position
-        input.setSelectionRange(adjustedCursorPosition, adjustedCursorPosition);
-    }
 </script>
 
 <div class="grid items-center justify-items-center md:p-6">
@@ -66,55 +71,109 @@
         <div class="grid grid-flow-row gap-y-2 items-center justify-items-center">
             <Avatar {initials} />
             <h1 class="text-2xl font-bold">{data.user.name}</h1>
+            <!-- Email -->
             <div class="grid grid-flow-col min-w-full items-center gap-x-2 text-gray-500">
-                {#if edit_mode}
-                    <div class="relative">
-                        <Fa
-                            icon={faEnvelope}
-                            class="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
-                        />
-                        <input
-                            name="email"
-                            value={data.user.email}
-                            class="pl-8 border shadow-sm rounded-md p-1 text-gray-700 min-w-full"
-                        />
-                    </div>
-                {:else}
-                    <div class="grid grid-cols-[auto_1fr] items-center gap-x-2">
-                        <Fa icon={faEnvelope} class=" text-gray-400" />
-                        <p>{data.user.email}</p>
-                    </div>
-                {/if}
+                <div class="relative">
+                    <Fa
+                        icon={faEnvelope}
+                        class="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
+                    />
+                    <input
+                        disabled={!edit_mode}
+                        name="email"
+                        value={data.user.email}
+                        class="pl-8 border shadow-sm rounded-md p-1 text-gray-700 min-w-full disabled:bg-surface-50 disabled:text-black dark:disabled:bg-surface-900 dark:disabled:border-none dark:disabled:text-gray-50 dark:bg-surface-700 dark:border-surface-900 dark:border-none dark:text-gray-200"
+                    />
+                </div>
             </div>
+            <!-- Phone -->
             <div
                 class="grid grid-flow-col grid-cols-[auto_1fr] items-center gap-x-2 text-gray-500 min-w-full"
             >
-                {#if edit_mode}
-                    <div class="relative min-w-full">
-                        <input
-                            type="tel"
-                            value={phone}
-                            id="phone-input"
-                            class="border border-surface-100 shadow-sm rounded-md pl-8 p-1 text-gray-700 min-w-full"
-                            on:input={formatPhoneNumber}
-                        />
-                    </div>
-                {:else}
-                    <Fa icon={faPhone} class="left-2 text-gray-400" />
-                    <p class="text-gray-500">{phone}</p>
-                {/if}
+                <div class="relative min-w-full">
+                    <Fa
+                        icon={faPhone}
+                        class="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
+                    />
+                    <input
+                        disabled={!edit_mode}
+                        type="tel"
+                        value={phone}
+                        id="phone-input"
+                        maxlength="12"
+                        on:input={updatePhoneNumber}
+                        class="border border-surface-100 shadow-sm rounded-md pl-8 p-1 text-gray-700 min-w-full disabled:bg-surface-50 disabled:text-black dark:disabled:bg-surface-900 dark:disabled:border-none dark:disabled:text-gray-50 dark:bg-surface-700 dark:border-surface-900 dark:border-none dark:text-gray-200"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <hr class="min-w-full mt-4" />
+
+        <div class="grid grid-flow-row gap-y-1 items-center justify-items-center">
+            <!-- Bio -->
+            <div
+                class="grid grid-flow-col grid-cols-[auto_1fr] items-center gap-x-2 text-gray-500 min-w-full"
+            >
+                <div class="min-w-full">
+                    <p class="dark:text-gray-400 text-gray-600 h-auto font-bold">Bio</p>
+                    <textarea
+                        disabled={!edit_mode}
+                        value={bio}
+                        id="phone-input"
+                        class="border border-surface-100 shadow-sm rounded-md p-2 text-gray-700 min-w-full resize-none h-24 disabled:bg-surface-50 disabled:text-black dark:disabled:bg-surface-900 dark:disabled:border-none dark:disabled:text-gray-50 dark:bg-surface-700 dark:border-surface-900 dark:border-none dark:text-gray-200"
+                    />
+                </div>
+            </div>
+
+            <!-- City -->
+            <div class="min-w-full">
+                <p class="dark:text-gray-400 text-gray-600 h-auto font-bold">City</p>
+                <input
+                    disabled={!edit_mode}
+                    value={city}
+                    id="phone-input"
+                    class="border border-surface-100 shadow-sm rounded-md pl-2 p-1 text-gray-700 min-w-full disabled:bg-surface-50 disabled:text-black dark:disabled:bg-surface-900 dark:disabled:border-none dark:disabled:text-gray-50 dark:bg-surface-700 dark:border-surface-900 dark:border-none dark:text-gray-200"
+                />
+            </div>
+
+            <!-- State -->
+            <div class="min-w-full">
+                <p class="dark:text-gray-400 text-gray-600 h-auto font-bold">State</p>
+                <input
+                    disabled={!edit_mode}
+                    value={state}
+                    id="phone-input"
+                    class="border border-surface-100 pl-2 shadow-sm rounded-md p-1 text-gray-700 min-w-full disabled:bg-surface-50 disabled:text-black dark:disabled:bg-surface-900 dark:disabled:border-none dark:disabled:text-gray-50 dark:bg-surface-700 dark:border-surface-900 dark:border-none dark:text-gray-200"
+                />
             </div>
         </div>
     </div>
-    <div class="grid grid-cols-2 gap-x-4">
-        <button disabled class="btn btn-sm variant-soft mt-4">Cancel</button>
+
+    {#if edit_mode}
+        <div class="grid grid-flow-col gap-x-2 items-center justify-self-end">
+            <button
+                class="btn btn-sm mt-4 justify-self-end text-gray-700 dark:text-gray-500"
+                on:click={cancelChanges}
+            >
+                Cancel
+            </button>
+            <button
+                class="btn btn-sm variant-filled-primary mt-4 justify-self-end"
+                on:click={saveChanges}
+            >
+                Save Changes
+            </button>
+        </div>
+    {:else}
         <button
-            class="btn btn-sm variant-filled-primary mt-4"
+            class="btn btn-sm variant-soft mt-4 justify-self-end"
             on:click={() => {
                 edit_mode = !edit_mode;
             }}
         >
+            <Fa icon={faPenToSquare} class="mr-2" />
             Edit Profile
         </button>
-    </div>
+    {/if}
 </div>
