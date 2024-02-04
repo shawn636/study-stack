f0b2de04dbc89afaa9e1191089aa899331ff0715
Shawn Long <shawnlong636@gmail.com>
2024-02-04 15:55:03 -0800

    feat: added error handling to session-cleanup cron method

--- src/routes/api/cron/session-cleanup/+server.ts
+++ src/routes/api/cron/session-cleanup/+server.ts
@@ -1,25 +1,33 @@
 import type { RequestHandler } from './$types';
 import { db } from '$lib/server/database';
 
-export const GET = (async () => {
-    const conn = db.connection();
+export const GET: RequestHandler = async () => {
+    try {
+        const conn = db.connection();
 
-    const csrf_flush = await conn.execute(
-        'DELETE FROM csrf_token WHERE CAST(expires AS DATETIME) <= CURRENT_TIMESTAMP();'
-    );
+        const csrf_flush = await conn.execute(
+            'DELETE FROM csrf_token WHERE CAST(expires AS DATETIME) <= CURRENT_TIMESTAMP();'
+        );
 
-    const auth_session_flush = await conn.execute(
-        'DELETE FROM auth_session WHERE expires <= CURRENT_TIMESTAMP();'
-    );
+        const auth_session_flush = await conn.execute(
+            'DELETE FROM auth_session WHERE expires <= CURRENT_TIMESTAMP();'
+        );
 
-    const json = JSON.stringify({
-        csrf_tokens_flushed: csrf_flush.rowsAffected,
-        auth_sessions_flushed: auth_session_flush.rowsAffected
-    });
+        const json = JSON.stringify({
+            csrf_tokens_flushed: csrf_flush.rowsAffected,
+            auth_sessions_flushed: auth_session_flush.rowsAffected
+        });
 
-    return new Response(json, {
-        headers: {
-            'content-type': 'application/json;charset=UTF-8'
-        }
-    });
-}) satisfies RequestHandler;
+        return new Response(json, {
+            headers: {
+                'content-type': 'application/json;charset=UTF-8'
+            },
+            status: 200
+        });
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return new Response('Internal Server Error', {
+            status: 500
+        });
+    }
+};
