69c03be5c62d73d1cc92c4679df8203630aa6a12
Shawn Long <shawnlong636@gmail.com>
2024-04-02 21:33:10 -0700

    feat: added db health check in root layout server load

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,18 @@
+import { db, sql } from '$lib/server/database';
+
+import type { LayoutServerLoad } from './$types';
+
+export const load = (async () => {
+    let databaseConnectionSuccessful = false;
+
+    try {
+        const result = await sql<{ solution: number }>`select 1 + 1 as solution`.execute(db);
+        if (result.rows.length === 1 && Number(result.rows[0].solution) === 2) {
+            databaseConnectionSuccessful = true;
+        }
+    } catch (error) {
+        console.error('Error: Unable to establish connection between server and database');
+    }
+
+    return { databaseConnectionSuccessful };
+}) satisfies LayoutServerLoad;
