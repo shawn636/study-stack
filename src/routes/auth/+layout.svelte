77bc91c37007670cb58d9f2ca5af66addb82ea20
Shawn Long <shawnlong636@gmail.com>
2024-04-26 19:15:00 -0700

    feat: fixed initialization of containsInvalidParams and removed commented out code

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import Logo from '$lib/components/logo.svelte';
-    // import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="grid min-h-screen md:grid-cols-[2fr_1fr]" data-testid="app-shell">
@@ -13,16 +12,3 @@
         <Logo layout="vertical" />
     </div>
 </div>
-<!-- <AppShell
-    slotSidebarRight="bg-gradient-to-tr from-primary-700 to-primary-500 w-64 hidden md:block rounded-l-3xl shadow-xl"
->
-    <div class="container grid h-full grid-flow-row items-center justify-items-center">
-        <slot />
-    </div>
-
-    <svelte:fragment slot="sidebarRight">
-        <div class="grid h-full items-center justify-items-center">
-            <Logo layout="vertical" />
-        </div>
-    </svelte:fragment>
-</AppShell> -->
