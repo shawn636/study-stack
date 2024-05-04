af93a224459d921dce07667a2e8430350dfbf93b
Shawn Long <shawnlong636@gmail.com>
2024-05-04 08:46:08 -0700

    feat: replaced accidentally deleted page

---
+++ src/routes/(logged_in)/profile/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageServerData } from './$types';
+
+    import ProfileForm from './profile-form.svelte';
+
+    export let data: PageServerData;
+</script>
+
+<ProfileForm user={data.user} />
