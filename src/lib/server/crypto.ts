b9fec56fd3c100181b5f70e3d06b248869e3eb5d
Shawn Long <shawnlong636@gmail.com>
2024-07-20 23:08:01 -0700

    fix: updated the dev server command to use op

--- src/lib/server/crypto.ts
+++ src/lib/server/crypto.ts
@@ -10,6 +10,7 @@ import bcrypt from 'bcryptjs';
 export const hashPassword = async (password: string): Promise<string> => {
     const salt = await bcrypt.genSalt();
     const pepperedPassword = password + PEPPER;
+
     return await bcrypt.hash(pepperedPassword, salt);
 };
 
@@ -22,6 +23,7 @@ export const hashPassword = async (password: string): Promise<string> => {
  */
 export const comparePassword = async (password: string, hash: string): Promise<boolean> => {
     const pepperedPassword = password + PEPPER;
+    console.debug('Pepper', PEPPER);
     return await bcrypt.compare(pepperedPassword, hash);
 };
 
