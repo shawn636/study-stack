f89258eaec8077f0bdeb7cd5a4a0f2fa0d107430
Shawn Long <shawnlong636@gmail.com>
2024-10-14 21:53:07 -0700

    fix: set op run from github actions to not specify an env file

--- scripts/shared/run.sh
+++ scripts/shared/run.sh
@@ -8,7 +8,13 @@ source scripts/pscale/common.sh
 
 if [[ "$USE_OPCLI" == "true" ]]; then
     echo "Running command with onepassword cli"
-    op run --env-file="./.env" -- "$@"
+
+    # CHECK IF IN GITHUB ACTIONS
+    if [[ -n "$CI" ]]; then
+        op run -- "$@"
+    else
+        op run --env-file="./.env" -- "$@"
+    fi
 else
     echo "Running command without onepassword cli"
     exec "$@"
